1ace07362727ab4e0b65627d1b2de3bbafaea518
Alistair Rowden <deadlyhardware@gmail.com>
2021-12-14 00:03:35 -0500

    :tada: Change onkeyup function to use the eventListener method

--- assets/javascript/clickHandlers.js
+++ assets/javascript/clickHandlers.js
@@ -1,3 +1,3 @@
 startBtn.addEventListener("click", startGame);
 nextWordBtn.addEventListener("click", nextWord);
-document.onkeyup = e => canType && runUserGuess(e);
+document.addEventListener("keyup", (e) => canType && runUserGuess(e));
